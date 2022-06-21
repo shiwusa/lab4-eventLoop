21b5e8e6478ab5d3abc1bb8f87f6055184f517c0
Vitaliy Yanyshyn <alkatel-3@ukr.net>
2022-06-21 22:14:39 +0300

    Update loop.go

--- engine/loop.go
+++ engine/loop.go
@@ -1,47 +1,45 @@
-package engine
-
 type Loop struct {
-	sync.Mutex
-	q          *commandsQueue
-	stop       bool
-	busy       bool
-	stopSignal chan bool
-}
+		sync.Mutex
+		q          *commandsQueue
+		stop       bool
+		busy       bool
+		stopSignal chan bool
+	}
 
-func (l *Loop) Post(cmd Command) {
-	l.Lock()
-	defer l.Unlock()
-	l.q.enqueue(cmd)
-	if l.busy && !l.stop {
+	func (l *Loop) Post(cmd Command) {
+		l.Lock()
+		defer l.Unlock()
+		l.q.push(cmd)
+		if l.busy && !l.stop {
+			l.startRoutine()
+		}
+	}
+
+	func (l *Loop) startRoutine() {
+		l.busy = false
+		go func() {
+			for {
+				if l.q.length() > 0 {
+					cmd := l.q.pull()
+					cmd.Execute(l)
+				} else if l.stop {
+					l.stopSignal <- true
+					return
+				} else {
+					l.Lock()
+					defer l.Unlock()
+					l.busy = true
+					return
+				}
+			}
+		}()
+	}
+
+	func (l *Loop) Start() {
+		l.stopSignal = make(chan bool, 1)
+		l.q = &commandsQueue{}
 		l.startRoutine()
 	}
-}
-
-func (l *Loop) startRoutine() {
-	l.busy = false
-	go func() {
-		for {
-			if l.q.length() > 0 {
-				cmd := l.q.dequeue()
-				cmd.Execute(l)
-			} else if l.stop {
-				l.stopSignal <- true
-				return
-			} else {
-				l.Lock()
-				defer l.Unlock()
-				l.busy = true
-				return
-			}
-		}
-	}()
-}
-
-func (l *Loop) Start() {
-	l.stopSignal = make(chan bool, 1)
-	l.q = &commandsQueue{}
-	l.startRoutine()
-}
 
 type stopCommand struct{}
 
