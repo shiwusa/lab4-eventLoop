7b36b5f1f166ecb70ff816cf93875a1f223dfd86
Vitaliy Yanyshyn <alkatel-3@ukr.net>
2022-06-21 18:53:32 +0300

    Delete the routine employment check
    Verification does not make sense because each command is performed in its own routine

--- engine/commandsQueue.go
+++ engine/commandsQueue.go
@@ -3,41 +3,27 @@ package engine
 import "sync"
 
 type commandsQueue struct {
-	mu       sync.Mutex
+	sync.Mutex
 	commands []Command
-	wait     bool
-
-	notEmpty chan struct{}
 }
 
-func (cq *commandsQueue) push(c Command) {
-	cq.mu.Lock()
-	defer cq.mu.Unlock()
-	cq.commands = append(cq.commands, c)
-	if cq.wait {
-		cq.notEmpty <- struct{}{}
-	}
+func (cq *commandsQueue) push(cmd Command) {
+	cq.Lock()
+	defer cq.Unlock()
+	cq.commands = append(cq.commands, cmd)
 }
 
 func (cq *commandsQueue) pull() Command {
-	cq.mu.Lock()
-	defer cq.mu.Unlock()
-
-	if len(cq.commands) == 0 {
-		cq.wait = true
-		cq.mu.Unlock()
-		<-cq.notEmpty
-		cq.mu.Lock()
-	}
-
+	cq.Lock()
+	defer cq.Unlock()
 	res := cq.commands[0]
 	cq.commands[0] = nil
 	cq.commands = cq.commands[1:]
 	return res
 }
 
-func (cq *commandsQueue) empty() bool {
-	cq.mu.Lock()
-	defer cq.mu.Unlock()
-	return len(cq.commands) == 0
+func (cq *commandsQueue) length() int {
+	cq.Lock()
+	defer cq.Unlock()
+	return len(cq.commands)
 }
